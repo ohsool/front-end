7574e5015d7a6ceda5f43cdc60067d2ddfc0c11a
endol007 <37258336+endol007@users.noreply.github.com>
2021-07-29 11:20:11 +0900

    redux middleware history

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {createGlobalStyle} from "styled-components";
+import {Route} from "react-router-dom";
+import { history } from "./redux/configureStore";
+import { ConnectedRouter } from "connected-react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyle/>
+      <ConnectedRouter history={history}>
+
+      </ConnectedRouter>
+    </React.Fragment>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: "Noto Sans KR", sans-serif;
+    background-color: #F2F3F7;
+  }
+`;
