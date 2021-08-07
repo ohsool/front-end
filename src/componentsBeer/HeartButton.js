599b282e056938e74fb88766cef2b4570e2168c2
endol007 <37258336+endol007@users.noreply.github.com>
2021-08-07 13:23:51 +0900

    change heart image

--- src/componentsBeer/HeartButton.js
+++ src/componentsBeer/HeartButton.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-import heart_red from "../share/image/heart_red.jpeg";
-import heart_black from "../share/image/heart_black.png";
+import heart_filled from "../share/image/heart_filled.png";
+import heart_empty from "../share/image/heart_black.png";
+import detail_heart from "../share/image/heart_detail.png";
 
 const HeartButton = ({_onClick, heart_detail, is_like }) => {
-  const icon_url = is_like? heart_red : heart_black;
+  const icon_url = is_like? heart_filled : heart_empty;
+  const icon_url_detal = is_like? detail_heart : heart_empty;
 
   return (
     <React.Fragment>
@@ -13,7 +15,7 @@ const HeartButton = ({_onClick, heart_detail, is_like }) => {
       <Heart onClick={_onClick} src={icon_url}></Heart>
       ) :
       (
-      <HeartDetail onClick={_onClick} src={icon_url}></HeartDetail>
+      <HeartDetail onClick={_onClick} src={icon_url_detal}></HeartDetail>
       )}
     </React.Fragment>
   );
