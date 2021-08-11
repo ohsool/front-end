bbc1b338602e79cc62167a5c36c81ed197ac6b0f
endol007 <37258336+endol007@users.noreply.github.com>
2021-08-11 10:22:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const Kakao = window.Kakao;
 
 useEffect(() => {// 만약 공유 기능이 2개이상으로 바뀌면 kakao.link.createdefaultbutton 사용하기 (그때는 내용에 container가 포함 되어있아야한다)
-    Kakao.init("05e106dead8f4edabc80bafcaef225ce");
+    Kakao.init("AppKey");
 }, []);
 
 /*
