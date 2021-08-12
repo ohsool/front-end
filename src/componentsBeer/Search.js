7859a2086834f2f824c0b3e2190cdbf27e645ba4
Jeongyun Jang <jjy306105@gmail.com>
2021-08-12 22:27:44 +0900

    recent search words

--- src/componentsBeer/Search.js
+++ src/componentsBeer/Search.js
@@ -1,11 +1,11 @@
-import React,{ useState } from "react";
+import React,{ useState, useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { getSearchWord } from "../redux/async/beer";
 import { getSearchList } from "../redux/reducer/beerSlice";
-
 const Search = (props) => {
-    const { setSearch_Beer, 
+    const { setSearch_Beer,
+            search_beer,
             beers, 
             setIs_Search
             } = props;
@@ -13,47 +13,52 @@ const Search = (props) => {
     const check_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; // 한글체크
     const [word, setWord] = useState(""); //실시간으로 입력하는 단어담김
     const words = useSelector(getSearchList);
+    const [show_recent_words, setShow_Recent_Words] = useState(false);//최근 검색어 보여줄지, 실시간 자동완성 검색어 보여줄지
+    const [recent_words, setRecent_Words] = useState(localStorage.getItem("recent_words").split(','));//검색한 글자 최근 검색 리스트에 추가
     const dispatch = useDispatch();
-
+    useEffect(()=>{
+        if(recent_words === null){
+            setRecent_Words("");
+        }
+    },[recent_words])
     const onChange = (e) =>{
         if(e.target.value === ''){//검색어 지웠을 때 검색목록 사라지도록 함
-            setWord(null) 
+            setWord(null);
+            setShow_Recent_Words(true);
         }else{
             setWord(e.target.value);
+            setShow_Recent_Words(false);
         }    
     }
-
-    const searchWord = () =>{
+    const searchWord = () =>{//실시간으로 자동완성 된 값 불러옴   
         dispatch(getSearchWord(word)); 
     }
-
     const EnterSubmit = (e) =>{
         if(e.key === "Enter"){
             findBeer();
         }
     }
-
-
-    const findBeer = ()=>{
+    const findBeer = ()=>{//엔터 키를 누른 경우 해당 단어로 검색
         setSearch_Beer([]);
-        if(check_eng.test(word)){//영어로 검색           
+        if(check_eng.test(word) && show_recent_words === false){//영어로 검색           
             words.map((w)=>{
                 setSearch_Beer(search_beer => [...search_beer,
                 beers?.filter((p) => p.name_korean.includes(w))[0]]);
                 setIs_Search(true);
             })
-        }else if(check_kor.test(word)){//한국어로 검색            
+        }else if(check_kor.test(word) && show_recent_words === false){//한국어로 검색            
             words.map((w)=>{
                 setSearch_Beer(search_beer => [...search_beer,
                 beers?.filter((p) => p.name_korean.includes(w))[0]]);
-                // setSearch_Beer();
                 setIs_Search(true);
             })
         }else{
             window.alert("잘못 입력 하셨습니다.");
         }
+        setRecent_Words(recent_words =>[...recent_words, word]);//최근 검색어 리스트에 저장
+        localStorage.setItem("recent_words", recent_words.concat(word) );//최근 검색어 리스트에 저장
     }
-    const findBeerbyClick = (name)=>{
+    const findBeerbyClick = (name)=>{//특정 맥주명을 누른 경우 해당 맥주 명으로 검색
         setSearch_Beer([]);
         if(check_eng.test(word)){//영어로 검색            
             setSearch_Beer(search_beer => [...search_beer, 
@@ -62,51 +67,61 @@ const Search = (props) => {
             setSearch_Beer(search_beer => [...search_beer, 
             beers?.filter((p) => p.name_korean.includes(name))[0]]);
         }
-        setIs_Search(true);        
+        setIs_Search(true);
+        localStorage.setItem("recent_words", recent_words.concat(search_beer[0]?.name_korean));//최근 검색어 리스트에 저장
+        //최근 검색어 리스트에 저장
+        //모달 close
     }
-  
+/*
+    const showRecentWords = () => {
+        console.log("word.lenght",words.length);
+        console.log("recent_words",recent_words);
+        if(words.length === 0){
+            return (
+                    <SearchModal>
+                        {recent_words?.length > 0 ? recent_words?.map((item, idx) => {
+                            return (
+                            <p key={idx} onClick={()=>{
+                                findBeerbyClick(item);
+                                }}>{item}</p> 
+                            )       
+                        }):""}                                          
+                    </SearchModal>
+            )
+        }
+    }
+ */   
     return (
         <React.Fragment>
             <SearchInput>
-                <input 
+                <input
+                    onClick={()=>{
+                        console.log("onClick!")
+                        //showRecentWords();
+                    }} 
                     onChange={onChange}
                     onKeyUp={searchWord}
                     onKeyPress={EnterSubmit}
                     placeholder="검색어를 입력하세요."
                 ></input>
-                
             </SearchInput>
-
-                     
             { words.length !== 0 ?
-                <SearchModal>
-                    <div style={{paddingLeft: "40px"}}>
+                    <SearchModal>
                     {words?.length > 0 ? words.map((item, idx) => {
                         return (
-                            <p
-                               key={idx} onClick={()=>{
-                               findBeerbyClick(item);
-                            }}>{item}
-                            </p> 
+                        <p key={idx} onClick={()=>{
+                            findBeerbyClick(item);
+                            }}>{item}</p> 
                         )       
-                    }):""}            
-                    </div>
-                </SearchModal>
-                : null }
-
-
-
+                    }):""}                                          
+                    </SearchModal>
+            : null }
         </React.Fragment>
     )
-
-
-
 }
 export default React.memo(Search);
-
 const SearchInput = styled.div`
     width: 360px;
-    
     & > input{
         width: 292px;
         height: 30px;
@@ -118,21 +133,22 @@ const SearchInput = styled.div`
         padding-left: 20px;
     }
 `
-
 const SearchModal = styled.div`
-    position: absolute;
-    margin : 0 auto;    
-    width: 360px;
+    display: inline-block;
+    padding-left: 26px;
+    height: 100vh; 
     background-color: #FFFFFF;
-    
     //글자 라인 수 제한하기
-    overflow: scroll;
+    overflow: hidden;
     display: -webkit-box;
-    min-height: 70px;
     -webkit-line-clamp: 5;
     -webkit-box-orient: vertical;
+    & > p {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 20.51px;
+    }
 `
-
 const CloseIcon = styled.div`
     position: absolute;
     right: 24px;
