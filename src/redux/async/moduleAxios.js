7160c0e25f838f7863803ffb4c4689881140710c
endol007 <endol007@naver.com>
2021-08-24 20:36:37 +0900

    pull

--- src/redux/async/moduleAxios.js
+++ src/redux/async/moduleAxios.js
@@ -44,7 +44,7 @@ axiosInstance.interceptors.request.use(
         return response;
     }, async function (error) {
         const originalRequest = error.config;
-        if(error.response.status === 401 && !originalRequest._retry){
+        if(error.response.status === 418 && !originalRequest._retry){
             originalRequest._retry = true;
             const response = await axios.get('/api/user/me')
             if(response.data.dhtnf){
